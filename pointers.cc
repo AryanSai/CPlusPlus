5ca91bb5701f203eabc6523c0f5446ac26584342
Aryan Sai Arvapelly <aryansaiarvapelly@gmail.com>
2024-11-05 15:12:06 +0530

    pointers

--- pointers.cc
+++ pointers.cc
@@ -3,25 +3,28 @@
 
 using namespace std;
 
-int main(){
-	int age=10;
+int main()
+{
+	int age = 10;
 	cout << age << endl;
 	cout << &age << endl;
+	
 	int *ageptr;
 	ageptr = &age;
 	cout << ageptr << endl;
+
 	int *newptr = &age;
 	cout << *newptr << endl;
 
-	 int value = 100;
-    	int *ptr = &value; // a pointer of integer type which stores the 'value' variable
-    	cout << value << "\n";
-    	cout << &value << "\n"; //&value gives the address of the variable 'value'
-    	cout << ptr << "\n";    //*ptr gives the address of the variable 'value'
-    	cout << *ptr << "\n";   // dereference to the value
+	int value = 100;
+	int *ptr = &value; // a pointer of integer type which stores the 'value' variable
+	cout << value << "\n";
+	cout << &value << "\n"; //&value gives the address of the variable 'value'
+	cout << ptr << "\n";	//*ptr gives the address of the variable 'value'
+	cout << *ptr << "\n";	// dereference to the value
 
-    	*ptr = 120;
-    	cout << *ptr << "\n"; // modify the value
-    	cout << value << "\n";
+	*ptr = 120;
+	cout << *ptr << "\n"; // modify the value
+	cout << value << "\n";
 	return 0;
 }
